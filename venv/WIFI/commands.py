108dd4063dc3475e062fe4b21218f2b9eff27add
Roman Smetana <rsmetana56@gmail.com>
2021-07-22 11:53:16 -0400

    who cares

--- venv/WIFI/commands.py
+++ venv/WIFI/commands.py
@@ -1,4 +1,17 @@
-with open('aps_commands.txt') as file:
-    for ap in file:
-        print(f'show ap auto-rf 802.11a {ap}')
+#with open('aps_commands.txt') as file:
+#    for ap in file:
+#        print(f'show ap auto-rf 802.11a {ap}')
+with open('aps_commands.txt' ) as a, open('mac_adr.txt') as b:
+    for ap in a:  
+        less3 = ap[3:]
+        new_names = [f'JFK875'+less3]
+        print(new_names)
+    
 
+
+
+
+
+
+
+        
