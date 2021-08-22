a5564c742cfad04402f9126d7107b5904dda0b5b
Roman Smetana <rsmetana56@gmail.com>
2021-08-22 12:58:18 -0400

    stuff

--- venv/WIFI/Flexconnect.py
+++ venv/WIFI/Flexconnect.py
@@ -1,4 +1,4 @@
 inventory = open('moveback.txt', 'r')
 for line in inventory:
-    print('config ap mode local ' + line.split()[0])
+    print('config ap mode local ' + line.strip())
     print('y')
