0e31bc574f5e46725695d0a0d64e035f9cc7e800
Roman Smetana <rsmetana56@gmail.com>
2022-04-04 17:57:07 -0400

    Stuff

--- venv/WIFI/Assign_group.py
+++ venv/WIFI/Assign_group.py
@@ -1,4 +1,4 @@
 inventory = open('JA_APs.txt', 'r')
 for line in inventory:
-    print('config ap group-name JH284 '+ line.strip())
+    print('config ap group-name JM292 '+ line.strip())
     print('y')
--- venv/WIFI/assign_cont.py
+++ venv/WIFI/assign_cont.py
@@ -1,6 +1,7 @@
 inventory = open('GLSC035.txt', 'r')
 for line in inventory:
-    print('config ap primary-base CORE-WLCC ' + line.strip() +' 10.160.0.7')
+    print('config ap primary-base CORE-WLCA ' + line.strip() +' 10.160.0.8')
+    print('config ap secondary-base CORE-WLCA ' + line.strip() +' 0.0.0.0')
 
 
 
